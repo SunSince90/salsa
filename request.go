04dbf7f2de5f0cc11e2624aeb030e440226e7750
Elis Lulja <elis.lulja@gmail.com>
2021-12-09 18:54:48 +0100

    Create makeRequest function
    This function performs the actual request to Saucenao with the given
    options.
    
    Signed-off-by: Elis Lulja <elis.lulja@gmail.com>

--- request.go
+++ request.go
@@ -2,11 +2,44 @@ package salsa
 
 import (
 	"bytes"
+	"encoding/json"
+	"fmt"
 	"io"
 	"mime/multipart"
 	"net/http"
+	"net/url"
 	"os"
 )
+
+func makeRequest(opts *RequestOptions) (sr *sauceResponse, err error) {
+	reqURL, _ := url.Parse(defaultBaseURL)
+	reqURL.RawQuery = buildQueries(opts).Encode()
+
+	var req *http.Request
+
+	if opts.file != nil {
+		req, err = createRequestForFile(reqURL.String(), *opts.file)
+	} else {
+		req, err = createRequestForURL(reqURL.String(), *opts.url)
+	}
+
+	if err != nil {
+		return nil, fmt.Errorf("could not prepare request for sauce: %w", err)
+	}
+
+	res, err := opts.HttpClient.Do(req)
+	if err != nil {
+		return nil, fmt.Errorf("error while making request: %w", err)
+	}
+
+	defer res.Body.Close()
+
+	if err := json.NewDecoder(res.Body).Decode(&sr); err != nil {
+		return nil, fmt.Errorf("could not parse response from server: %w", err)
+	}
+
+	return
+}
 
 func createRequestForURL(u, sauceURL string) (req *http.Request, err error) {
 	body := &bytes.Buffer{}
