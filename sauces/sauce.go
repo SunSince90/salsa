ee65112203cb08e1659c84c4eee9a61c988b2773
Elis Lulja <elis.lulja@gmail.com>
2021-12-11 19:33:08 +0100

    Create sauce interface
    Signed-off-by: Elis Lulja <elis.lulja@gmail.com>

---
+++ sauces/sauce.go
@@ -0,0 +1,19 @@
+package sauces
+
+import "github.com/SunSince90/salsa/indexes"
+
+// Sauce defines a general source returned by Saucenao.
+type Sauce interface {
+	// GetSimilarity returns the similarity percentage between the result
+	// and the provided image.
+	GetSimilarity() float64
+	// GetThumbnail returns a URL with the thumbnail of this image.
+	GetThumbnail() string
+	// IsHidden returns true if this result is hidden from the results.
+	IsHidden() bool
+	// GetIndex returns the index for this.
+	GetIndex() indexes.Index
+	// GetIndexName returns the index name or page title where this image is
+	// hosted.
+	GetIndexName() string
+}
